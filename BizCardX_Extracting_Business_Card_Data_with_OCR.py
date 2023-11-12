a339e3057ac92af66e1312a4bd0fe5b944945b2b
MUTHUKUMAR R <146977832+Muthukumar0908@users.noreply.github.com>
2023-11-12 10:53:22 +0530

    Add files via upload

--- BizCardX_Extracting_Business_Card_Data_with_OCR.py
+++ BizCardX_Extracting_Business_Card_Data_with_OCR.py
@@ -30,31 +30,22 @@ engine = create_engine(f'mysql+mysqlconnector://{user}:{password}@{host}/{databa
 
 def collect_data():
     st.title('Image Text Recognition')
-
-    # Upload image via Streamlit file uploader
     image = st.file_uploader('Upload an image', type=['jpg', 'jpeg', 'png']) 
 
     if image is not None:
-        # Display the uploaded image
         st.image(image, use_column_width=True)
-
-        # Process the image and extract text using easyocr
         with st.spinner('Recognizing text...'):
     
             image_data = image.read()
             result = reader.readtext(image_data,paragraph="False")
-
-                # Display recognized text
             if result:
-                st.subheader('Recognized Text:')
                 # st.write(result)
-            # else:
-            #     st.warning('No text detected in the image.')
                 for i in result:
                         a=str(i[-1])
                     #     print(type(a))
                         dic.append(a)
                         data1= ', '.join([str(item) for item in dic])
+                st.markdown("#### first step: :ok_hand:")
                 st.write(data1)
                 
                 sql_data={}
@@ -111,13 +102,17 @@ def collect_data():
                 sql_data['designation']=designation
                 # # # sql_data.append(city)
                 # print(type(sql_data))
+            st.markdown("#### second step:  :ok_hand: ")
             st.table(sql_data)
+            a=sql_data.keys()
             listq=[]
             for i in sql_data.values():
                 listq.extend(i)
             h=listq[0]
             df=pd.DataFrame(sql_data)
-            # df
+            df1=pd.DataFrame(columns=a)
+            table_name ='card_data'
+            df1.to_sql(table_name,if_exists="append" ,con=engine, index=False)
             if st.button('import to sql'):
                 query="""SELECT name FROM card_data"""
                 mycursor.execute(query)
@@ -134,56 +129,77 @@ def collect_data():
                 # st.write(a)
                 if h in a:
                     # print(listq)
-                    st.write('card data all ready exist')
+                    st.success('card data all ready exist !! :-1:')
                 else:
                     table_name ='card_data'
                     df.to_sql(table_name,if_exists="append" ,con=engine, index=False)
-                    st.write("new_card")
+                    st.success("new_card !!  :+1: ")
                     # st.table(sql_data)
 
-        
+def table_create():
+    mycursor.execute('USE bizcard')
+    mycursor.execute("""create table card_data """)
+    mydb.commit()
 # Initialize easyocr Reader
 reader = easyocr.Reader(['en']) 
 with st.sidebar:
     selected = st.selectbox("**Menu**", ["collect_data","edit_data"])
 if selected=="collect_data":
     collect_data()
-        
             
 if selected=='edit_data':
+    try:
         mycursor.execute("SELECT name,company_name FROM card_data")
         result = mycursor.fetchall()
         cards = {}
-        # print(business_cards)
+            # print(business_cards)
         for i in result:
             cards[i[0]] = i[0]
             # st.write(row[0])
         selected_card = st.selectbox("Select a card holder name to update", list(cards.keys()))
         st.markdown("#### Update or modify any data below")
-        mycursor.execute("select * from card_data WHERE name=%s",
-                        (selected_card,))
-        result = mycursor.fetchone()
-        name = st.text_input("name", result[6])
-        email= st.text_input("email", result[0])                             
-        website= st.text_input("website", result[1])
-        phonenumber=st.text_input("phonenumber", result[2])
-        pincode=st.text_input("pincode", result[3])
-        state=st.text_input("state", result[4])
-        city=st.text_input("address", result[5])
-        company_name=st.text_input("company_name", result[7])
-        designation=st.text_input("designation", result[8])
-        if st.button("ok"):
-            sq1l="update card_data set email=%s, website=%s,phonenumber=%s,pincode=%s,state=%s,city=%s,company_name=%s,designation=%s where name=%s"
-            values=(email,website,phonenumber,pincode,state,city,company_name,designation,name)
-            mycursor.execute(sq1l,values)
-            mydb.commit()
-            st.write("data was updated")
-    #   # if st.button("ok"):
-    #         # sq1l="update card_data set email=%s,website=%s,phonenumber=%s,picode=%s,state=%s,city=%s,company_name=%s,designation==%s where name=%s"
-    #         # values=(email,website,phonenumber,pincode,state,city,company_name,designation,name)
-    #         # mycursor.execute(sq1l,values)
-    #         # mydb.commit()
-    #         # st.success("Information updated in database successfully.")
+        colum4,colum6 = st.columns([0.5,0.5],gap="large")
+
+        with colum4:
+
+            
+            mycursor.execute("select * from card_data WHERE name=%s",
+                            (selected_card,))
+            result = mycursor.fetchone()
+            name = st.text_input("name", result[0])
+            email= st.text_input("email", result[1])                             
+            website= st.text_input("website", result[2])
+            phonenumber=st.text_input("phonenumber", result[3])
+            pincode=st.text_input("pincode", result[6])
+        with colum6:
+            
+            state=st.text_input("state", result[4],)
+            city=st.text_input("address", result[5])
+            company_name=st.text_input("company_name", result[7])
+            designation=st.text_input("designation", result[8])
+            st.markdown("####")
+            
+            st.markdown("####")
+        
+            st.markdown("####")
+        
+        if st.button("insert"):
+                sq1l="update card_data set email=%s, website=%s,phonenumber=%s,pincode=%s,state=%s,city=%s,company_name=%s,designation=%s where name=%s"
+                values=(email,website,phonenumber,pincode,state,city,company_name,designation,name)
+                mycursor.execute(sq1l,values)
+                mydb.commit()
+                st.success("data was updated!! :+1:")
+        if st.button("drop"):
+                dic=[]
+                sq1l="delete from card_data where name=%s"
+                dic.append(name)
+                values=(dic)
+                mycursor.execute(sq1l,values)
+                mydb.commit()
+                st.success("data was deleted !! :unamused:")
+    except:
+        st.success('no data added!!!!')            
+        
     
     
     
